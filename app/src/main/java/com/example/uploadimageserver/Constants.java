149964ba66dd43727fb75b281031f1b6e107870b
gs sagor <gssagor635@gmail.com>
2019-02-18 02:22:40 +0600

    18/2/19 first commit to upload image to server.

---
+++ app/src/main/java/com/example/uploadimageserver/Constants.java
@@ -0,0 +1,6 @@
+package com.example.uploadimageserver;
+
+public class Constants {
+    public static final String UPLOAD_URL = "http://192.168.0.17/UploadExample/upload.php";
+    public static final String IMAGES_URL = "http://192.168.0.17/UploadExample/getImages.php";
+}
